65de6f7cbf402f405a4a6c41065bb460369daa7c
keithrozario <keithjosephrozario@gmail.com>
2020-11-04 11:09:40 +0800

    Fixed env var naming issue

--- tests/load_credentials.py
+++ tests/load_credentials.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     platform_config = config.get_platform_config()
     creds_path = os.path.join(platform_config["aws_directory"], platform_config["creds_file_name"])
 
-    if len(os.environ.get('CREDENTIALS_FILE_CONTENTS', "") > 0:
+    if len(os.environ.get('CREDENTIALS_FILE_CONTENTS', "")) > 0:
             with open(creds_path,'w') as cred_file:
                 cred_file.write(os.environ.get('CREDENTIALS_FILE_CONTENTS', ""))
         print("env:CREDENTIALS found, loading")
