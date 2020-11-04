0aed7efae10d19d8989af3773da578f50425ac9f
keithrozario <keithjosephrozario@gmail.com>
2020-11-04 18:48:42 +0800

    safe print for windows

--- mentaws/main.py
+++ mentaws/main.py
@@ -32,31 +32,31 @@ def main():
             profile_name = sys.argv[2]
             remove(profile_name)
         except IndexError:
-            print(f"Please provide the profile to remove")
+            safe_print(f"Please provide the profile to remove")
             exit(1)
     else:
-        print(welcome_message)
+        safe_print(welcome_message)
     exit(0)
 
     return True
 
 
 def setup():
-    os.environ['PYTHONIOENCODING']='utf-8'
+
     profiles = setup_new_db()
     if len(profiles) > 0:
-        print(f"The following {len(profiles)} were added to mentaws:")
-        print(f"\n👷🏿 Profile{' ' * 20}")
+        safe_print(f"The following {len(profiles)} profiles were added to mentaws:")
+        safe_print(f"\n👷🏿 Profile{' ' * 20}".encode('ascii', 'ignore').decode('ascii'))
         for k, profile in enumerate(profiles):
-            print(f"{k+1:2}.{profile:<30}")
+            safe_print(f"{k+1:2}.{profile:<30}")
     return profiles
 
 
 def list_profiles():
     profiles = list_profiles_in_db()
-    print(f"\n👷🏿 Profile{' ' * 20}")
+    safe_print(f"\n👷🏿 Profile{' ' * 20}")
     for k, profile in enumerate(profiles):
-        print(f"{k+1:2}.{profile:<30}")
+        safe_print(f"{k+1:2}.{profile:<30}")
     return profiles
 
 
@@ -64,19 +64,19 @@ def refresh():
 
     new_profiles = check_new_profiles()
     if len(new_profiles) > 0:
-        print(
+        safe_print(
             f"Found {len(new_profiles)} new profiles in credentials file, added these to mentaws:"
         )
         for profile in new_profiles:
-            print(f"{profile}")
+            safe_print(f"{profile}")
 
     creds = get_plaintext_credentials()
 
     # Generate temp credentials
     temp_config = configparser.ConfigParser()
 
-    print("Generating temporary tokens...")
-    print(f"\n👷🏿 Profile{' ' * 20}🌎 Region:{' '*12}⏰ Tokens expire at")
+    safe_print("Generating temporary tokens...")
+    safe_print(f"\n👷🏿 Profile{' ' * 20}🌎 Region:{' '*12}⏰ Tokens expire at")
     for section in creds:
         region = get_region(profile=section["profile"])
         temp_token = get_token(
@@ -85,13 +85,13 @@ def refresh():
             region=region,
         )
         temp_config[section["profile"]] = temp_token
-        print(
+        safe_print(
             f"   {section['profile']:<30}{region:<22}{temp_token['aws_token_expiry_time']}"
         )
 
     # Replace ~/.aws/credentials
     write_creds_file(config=temp_config)
-    print(f"\n\nYou're ready to go 🚀🚀 ")
+    safe_print(f"\n\nYou're ready to go 🚀🚀 ")
 
     return
 
@@ -101,11 +101,11 @@ def remove(profile_name: str):
     if check_profile_in_db(profile_name):
         if yes_or_no(f"Are you sure you want to delete {profile_name}?"):
             remove_profile_from_db(profile_name)
-            print(f"Profile {profile_name} was deleted")
+            safe_print(f"Profile {profile_name} was deleted")
         else:
             pass
     else:
-        print(f"Profile {profile_name} not found")
+        safe_print(f"Profile {profile_name} not found")
 
     return list_profiles_in_db()
 
@@ -116,3 +116,13 @@ def yes_or_no(question):
         return True
     else:
         return False
+
+
+def safe_print(print_string: str)-> None:
+
+    try:
+        print(print_string)
+    except UnicodeEncodeError:
+        print(print_string.encode('ascii', 'ignore').decode('ascii'))
+
+    return None
