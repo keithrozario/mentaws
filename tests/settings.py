232d77bf65c177d43e487b83fbdc009c1b68efcf
keithrozario <keithjosephrozario@gmail.com>
2020-11-09 13:54:24 +0800

    changed bad key to a valid key

--- tests/settings.py
+++ tests/settings.py
@@ -2,6 +2,6 @@ from mentaws import config
 
 platform_config = config.get_platform_config()
 test_key = "VlBrGT5dCUh0IHW6WSU8-wdJEJbjCuUhAQ1HZn352Nk="
-bad_key = "VlBrGT5dCUh0IHW6WSU8-wdJEJbjCuUhAQ1HZn352N0="
+bad_key = "QUkb3rCxRW4YpybqVa_0SNBGsDDZQ2aOKjXtrrpRS8Y="  # invalid key
 num_profiles = 6
 profiles = ['default', 'mentaws1', 'mentaws2', 'mentaws3', 'mentawsFail']
