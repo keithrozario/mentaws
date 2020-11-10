7fa9760125464d959a2543a9587e228c7c9c4259
keithrozario <keithjosephrozario@gmail.com>
2020-11-10 13:41:04 +0800

    checked file contents

--- tests/test_99_unsetup.py
+++ tests/test_99_unsetup.py
@@ -1,5 +1,5 @@
 import os
-import filecmp
+import configparser
 
 import keyring
 
@@ -26,5 +26,19 @@ def test_unsetup(monkeypatch):
     assert os.path.isdir(settings.platform_config["aws_directory"]) == True
     assert os.path.exists(settings.creds_file_path) == True
     assert os.path.exists(settings.db_file_path) == False
-    assert filecmp.cmp(settings.creds_file_path,settings.copy_creds_file_path,shallow=False) == True
 
+def test_file_comparison():
+
+    new_config = configparser.ConfigParser()
+    new_config.read(settings.creds_file_path)
+
+    old_config = configparser.ConfigParser()
+    old_config.read(settings.copy_creds_file_path)
+
+    for section in old_config.sections():
+        for option in old_config.options(section):
+            assert old_config.get(section,option) == new_config.get(section, option)
+
+    for section in new_config.sections():
+        for option in new_config.options(section):
+            assert old_config.get(section,option) == new_config.get(section, option)
